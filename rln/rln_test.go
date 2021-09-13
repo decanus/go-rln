3482f3b88507c13ae78dcbb46b84544b338b053a
decanus <7621705+decanus@users.noreply.github.com>
2021-09-13 23:10:24 +0200

    added better funcs

--- rln/rln_test.go
+++ rln/rln_test.go
@@ -2,6 +2,7 @@ package rln_test
 
 import (
 	"io/ioutil"
+	"reflect"
 	"testing"
 
 	"github.com/decanus/go-rln/rln"
@@ -16,5 +17,30 @@ func TestNew(t *testing.T) {
 	_, err = rln.New(32, params)
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestGenerateKey(t *testing.T) {
+	params, err := ioutil.ReadFile("./testdata/parameters.key")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r, err := rln.New(32, params)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	k, err := r.GenerateKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if reflect.DeepEqual(k.Key, [32]byte{}) {
+		t.Fatal("k.Key was empty")
+	}
+
+	if reflect.DeepEqual(k.Commitment, [32]byte{}) {
+		t.Fatal("k.Commitment was empty")
 	}
 }
