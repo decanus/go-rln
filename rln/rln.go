7944678210c6da2faa8930ccc2134bb41ecfa1e5
Kobi Gurkan <kobigurk@gmail.com>
2021-09-13 22:58:27 +0300

    fix: pass pointer correctly

--- rln/rln.go
+++ rln/rln.go
@@ -14,11 +14,16 @@ type RLN struct {
 }
 
 func (r *RLN) Hash(input []byte) []byte {
-	out := &C.Buffer{}
+	size := int(unsafe.Sizeof(C.Buffer{}))
+	out_ptr := C.malloc(C.size_t(size))
 
-	in := toBuffer(input)
+	size = int(unsafe.Sizeof(C.Buffer{}))
+	in_ptr := C.malloc(C.size_t(size))
+  in := (*C.Buffer)(in_ptr)
+	*in = toBuffer(input)
 
-	C.hash(r.ptr, &in, &in.len, out)
+  out := (*C.Buffer)(out_ptr)
+	C.hash(r.ptr, in, &in.len, out)
 
 	return C.GoBytes(unsafe.Pointer(out.ptr), C.int(out.len))
 }
